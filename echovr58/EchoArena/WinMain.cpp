896577c4518b6a582055e63c1ee5fa357d7d0867
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2024-06-14 09:28:59 -0500

    echovr58: Cleanup WinMain

--- echovr58/EchoArena/WinMain.cpp
+++ echovr58/EchoArena/WinMain.cpp
@@ -5,15 +5,10 @@
 
 int __fastcall WinMain(HINSTANCE__ *hinst, HINSTANCE__ *previnstance, char *cmdline, int cmdshow)
 {
-    const char *CommandLineA;
-    unsigned int res;
-    int result;
-
     NRadEngine::CRT0::Init();
-    CommandLineA = GetCommandLineA();
-    res = NRadEngine::Main(CommandLineA);
+    const char *CommandLineA = GetCommandLineA();
+    unsigned int res = NRadEngine::Main(CommandLineA);
     NRadEngine::CSysOS::Shutdown();
-    result = res;
     NRadEngine::gLoggingInitialized = 0;
-    return result;
+    return (int)res;
 }
